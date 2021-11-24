932aee2284c96892bd856ab1c1187e6c6f1cad90
PedroCod <Pedro.hsoares131@gmail.com>
2021-11-23 21:19:46 -0300

    commit

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const Conn = require('./conn/conn');
 const TarefasRouter = require('./routes/tarefas.routes');
+const Conn = require('./conn/conn');
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
 
 app.use('/tarefas', TarefasRouter);
 
-
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
-}
-
+};
 const db_url = process.env.DB_URL;
 const db_user = process.env.db_user;
 const db_pass = process.env.DB_PASS;
 const db_data = process.env.DB_DATA;
 
-console.log(db_url, db_user, db_data, db_pass)
-
+console.log(db_url, db_user, db_data, db_pass);
 Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3001;
-
 app.listen(process.env.PORT || port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 })
